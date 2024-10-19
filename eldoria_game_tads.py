3b84832b2fee125451a4bdd644fc4e22eff8c7fd
amandamb <amandabraz.geo@gmail.com>
2024-10-18 21:09:16 -0300

    eldoria_game_tads-04
    Adicionada a etapa de entrar na caverna.

--- eldoria_game_tads.py
+++ eldoria_game_tads.py
@@ -5,15 +5,21 @@ import os
 from random import randint
 
 #Configurando o personagem
+#mimik = baú surpresa
+
 jogador = str(input('Digite o nome do aventureiro: '))
 nivel = 1
 experiencia = 0
 vida = 3
+tentativa = 0
 print(f'''Bem-vindo à aventura, {jogador}. Esse é seu quadro atual:
 Nível: {nivel}
 Experiência: {experiencia}
 Vida: {vida}''')
 
 caverna = str(input('Deseja entrar na caverna misteriosa? [s/n]\n '))
-
-
+if caverna == 's':
+    print('Bem-vindo à Caverna. Pode entrar!')
+else:
+    print('Você saiu da caverna!🏃‍♂️💨')
+    while True:
