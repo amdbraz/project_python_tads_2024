956ae80e3ebc516f14ce86123006cdba65eef9e4
amandamb <amandabraz.geo@gmail.com>
2024-10-18 22:13:14 -0300

    eldoria_game_tads-06
    Alteração da estrutura dos atributos jogador e monstros.

--- eldoria_game_tads.py
+++ eldoria_game_tads.py
@@ -8,13 +8,25 @@ import math
 #Configurando o personagem
 #mimik = baú surpresa
 #random = 0, 20
+nome = str(input('Digite o nome do aventureiro: '))
+
+#jogador = vida, esquiva, ataque, ataque crítico, defesa
+jogador = [nome, , ,] #definir valores dos atributos
+
+#Definição dos monstros -
+monstroF = ['Fácil', , ,] #definir valores dos atributos dos monstros
+monstroM = ['Médio', , ,]
+monstroD = ['Difícil', , ,]
+monstroB = ['Boss', , ,]
+
+
+
+
 
 jogador = str(input('Digite o nome do aventureiro: '))
 
-nivel = 1
-experiencia = 0
-vida = 3
-tentativa = 0
+
+
 print(f'''Bem-vindo à aventura, {jogador}. Esse é seu quadro atual:
 Nível: {nivel}
 Experiência: {experiencia}
