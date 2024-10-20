bd51fb3079a825a8c13c5df6d2f21bd5eafabf51
Gabriel Shinkae <gabriellshinkae@gmail.com>
2024-10-20 16:53:16 -0400

    eldoria_game_tads.py

--- eldoria_game_tads.py
+++ eldoria_game_tads.py
@@ -11,13 +11,13 @@ import math
 nome = str(input('Digite o nome do aventureiro: '))
 
 #jogador = vida, esquiva, ataque, ataque crítico, defesa
-jogador = [nome, , ,] #definir valores dos atributos
+jogador = [nome, 'Ataque','Defesa','vida', 'Esquiva'] #definir valores dos atributos
 
 #Definição dos monstros -
-monstroF = ['Fácil', , ,] #definir valores dos atributos dos monstros
-monstroM = ['Médio', , ,]
-monstroD = ['Difícil', , ,]
-monstroB = ['Boss', , ,]
+monstroF = ['Fracol','Ataque=3','Defesa=1','vida=8', 'Esquiva=2'] #definir valores dos atributos dos monstros
+monstroM = ['Médio', 'Ataque=4','Defesa=1','vida=12', 'Esquiva=4']
+monstroD = ['Difícil', 'Ataque=6','Defesa=2','vida=20', 'Esquiva=6']
+monstroC = ['Chefe', 'Ataque=10','Defesa=5','vida=45', 'Esquiva=8']
 
 print(f'Bem-vindo à aventura, {jogador}.')
 
