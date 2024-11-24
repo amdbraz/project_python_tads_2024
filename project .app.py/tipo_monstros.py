99a982c33ba2604c733bf227ca5ccba5c6c86783
Ellen Roberta <165433325+ellenrobertaa@users.noreply.github.com>
2024-11-24 18:19:02 -0300

    tipo_monstros.py
    Biblioteca dos monstros

---
+++ project .app.py/tipo_monstros.py
@@ -0,0 +1,7 @@
+monstros = {
+    "monstro_fraco": {"ataque": 10, "defesa": 1, "vida": 8, "esquiva": 2},
+    "monstro_medio": {"ataque": 4, "defesa": 1, "vida": 12, "esquiva": 4},
+    "monstro_forte": {"ataque": 6, "defesa": 2, "vida": 20, "esquiva": 6},
+    "monstro_dificil": {"ataque": 10, "defesa": 5, "vida": 45, "esquiva": 8},
+    "mimico": {"ataque": 4, "defesa": 1, "vida": 12, "esquiva": 4}
+}
